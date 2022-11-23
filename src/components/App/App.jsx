ada6a61adf9b9bcff87a55d721246084a4eaf4a1
Natali Ustinova <nata@purple.studio>
2022-11-23 16:06:16 +0200

    feat: promo desktop #26

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import Main from "../Main/Main";
+import Header from "../Header/Header";
+import React from "react";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Main />
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "../Header/Header";
+import Promo from "../Promo/Promo";
 
 const Main = () => {
   return (
     <main>
-      <Header />
+      <Promo />
     </main>
   );
 };
