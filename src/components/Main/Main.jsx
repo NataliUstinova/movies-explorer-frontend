4da87d561ab696933bc279e21f6cf1b048bcbbd1
Natali Ustinova <nata@purple.studio>
2022-11-24 20:29:45 +0200

    feat: footer #24

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,6 +5,7 @@ import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
 import Portfolio from "../Portfolio/Portfolio";
+import Footer from "../Footer/Footer";
 
 const Main = () => {
   return (
@@ -15,6 +16,7 @@ const Main = () => {
       <Techs />
       <AboutMe />
       <Portfolio />
+      <Footer />
     </main>
   );
 };
