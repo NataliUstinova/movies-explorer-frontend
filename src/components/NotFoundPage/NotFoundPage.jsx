05f2ff6939ce0ceb49acce9b3e50f546a39d27c0
Natali Ustinova <nata@purple.studio>
2022-11-25 15:50:09 +0200

    feat: not found  page #28

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "./NotFoundPage.css";
+import { useHistory } from "react-router-dom";
 
 const NotFoundPage = () => {
+  const history = useHistory();
   return (
-    <div>
-      <h1>Not found page</h1>
+    <div className="not-found">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__text">Страница не найдена</p>
+      <button className="not-found__back-button" onClick={history.goBack}>
+        Назад
+      </button>
     </div>
   );
 };
