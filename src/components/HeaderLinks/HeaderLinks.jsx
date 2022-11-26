e84364613139784ebec0cafbbbe0c896292982d4
Natali Ustinova <nata@purple.studio>
2022-11-26 14:05:58 +0200

    feat: header links classNames refactor #22

--- src/components/HeaderLinks/HeaderLinks.jsx
+++ src/components/HeaderLinks/HeaderLinks.jsx
@@ -10,37 +10,40 @@ const HeaderLinks = () => {
   return (
     <div>
       {isMainPage && !isLoggedIn && (
-        <nav className="header__links">
-          <Link className="header__link" to="/signup">
+        <nav className="header-links">
+          <Link className="header-links__link" to="/signup">
             Регистрация
           </Link>
-          <Link className="header__link header__button" to="/signin">
+          <Link
+            className="header-links__link header-links__button"
+            to="/signin"
+          >
             Войти
           </Link>
         </nav>
       )}
 
       {(!isMainPage || isLoggedIn) && (
-        <nav className="header__links header__links_movies">
+        <nav className="header-links header-links_movies">
           <Link
-            className={`header__link header__link_movies ${
-              isMainPage && "header__link_movies-white"
+            className={`header-links__link header-links__link_movies ${
+              isMainPage && "header-links__link_movies-white"
             }`}
             to="/movies"
           >
             Фильмы
           </Link>
           <Link
-            className={`header__link header__link_movies ${
-              isMainPage && "header__link_movies-white"
+            className={`header-links__link header-links__link_movies ${
+              isMainPage && "header-links__link_movies-white"
             }`}
             to="/saved"
           >
             Сохранённые фильмы
           </Link>
-          <Link className="header__account-button" to="/profile">
-            <p className="header__account-button-text">Аккаунт</p>
-            <div className="header__account-icon" />
+          <Link className="header-links__account-button" to="/profile">
+            <p className="header-links__account-button-text">Аккаунт</p>
+            <div className="header-links__account-icon" />
           </Link>
         </nav>
       )}
