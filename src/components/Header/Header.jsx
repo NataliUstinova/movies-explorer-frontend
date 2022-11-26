b11b4b38b5bf0ab637103303706f9edac8bc2e34
Natali Ustinova <nata@purple.studio>
2022-11-26 13:35:28 +0200

    feat: header resize logic #29

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import Logo from "../Logo/Logo";
+import Burger from "../Burger/Burger";
 
-const Header = ({ isMainHeader }) => {
+const MoviesHeader = ({ isMainHeader }) => {
+  //TODO временная переменная для состояния логина
+  const isLoggedIn = true;
   return (
-    <header className={`header ${!isMainHeader && "movies-header"}`}>
-      <Logo />
-      {isMainHeader && (
+    <div>
+      {isMainHeader && !isLoggedIn && (
         <nav className="header__links">
           <Link className="header__link" to="/signup">
             Регистрация
@@ -17,12 +19,23 @@ const Header = ({ isMainHeader }) => {
           </Link>
         </nav>
       )}
-      {!isMainHeader && (
+
+      {(!isMainHeader || isLoggedIn) && (
         <nav className="header__links header__links_movies">
-          <Link className="header__link header__link_movies" to="/movies">
+          <Link
+            className={`header__link header__link_movies ${
+              isMainHeader && "header__link_movies-white"
+            }`}
+            to="/movies"
+          >
             Фильмы
           </Link>
-          <Link className="header__link header__link_movies" to="/saved">
+          <Link
+            className={`header__link header__link_movies ${
+              isMainHeader && "header__link_movies-white"
+            }`}
+            to="/saved"
+          >
             Сохранённые фильмы
           </Link>
           <Link className="header__account-button" to="/profile">
@@ -30,6 +43,35 @@ const Header = ({ isMainHeader }) => {
             <div className="header__account-icon" />
           </Link>
         </nav>
+      )}
+    </div>
+  );
+};
+
+const Header = ({ isMainHeader }) => {
+  //TODO временная переменная для состояния логина
+  const isLoggedIn = true;
+  const mediaQuery = window.matchMedia("(max-width: 768px)").matches;
+  const [isSmallScreen, setIsSmallScreen] = useState();
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+  });
+  useEffect(() => {
+    if (mediaQuery) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  }, [width]);
+
+  return (
+    <header className={`header ${!isMainHeader && "movies-header"}`}>
+      <Logo />
+      {isSmallScreen ? (
+        <Burger isMainHeader={isMainHeader} />
+      ) : (
+        <MoviesHeader isMainHeader={isMainHeader} />
       )}
     </header>
   );
